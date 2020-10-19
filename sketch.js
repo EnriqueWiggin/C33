3ff8ea35effd870886935f45cb6e6db21c77f694
ewiggin <enriquewigginsjr@gmail.com>
2020-10-18 20:52:27 -0400

    debugged code

--- sketch.js
+++ sketch.js
@@ -7,14 +7,17 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var birds=[];
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
 var score = 0;
 
 function preload() {
     getBackgroundImg();
+    birdFlySound=loadSound("Fly.mp3.mp3");
+    birdSelectSound=loadSound("select.mp3.mp3")
 }
+
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -41,6 +44,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
+    bird2= new Bird(150,170);
+    bird3 = new Bird(100,170);
+    bird4 = new Bird(50,170);
+
+    birds.push(bird4);
+    birds.push(bird3);
+    birds.push(bird2);
+    birds.push(bird);
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
@@ -75,26 +86,42 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
+    bird3.display();
+    bird4.display();
     platform.display();
     //log6.display();
     slingshot.display();    
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(birds[birds.length-1].body, {x: mouseX , y: mouseY});
+
+        Matter.Body.applyForce(birds[birds.length-1].body,birds[birds.length-1].body.position,{x: 5 , y: -5})
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    birds.pop();
     gameState = "launched";
+    birdFlySound.play();
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
+    if((keyCode === 32) && gameState==="launched"){
+
+        if(birds.length>=0){
+        Matter.Body.setPosition(birds[birds.length-1].body,{x:200,y:50})
+
+       slingshot.attach(birds[birds.length-1].body);
+       gameState="onSling"
+       
+        }
+        bird.trajectory=[];
+        birdSelectSound.play();
     }
 }
 
